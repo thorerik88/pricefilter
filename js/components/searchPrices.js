b6309a91ccc03636e5d8f3c51c4b21c0398a28a5
thorerik88 <thorerik88@hotmail.com>
2021-01-08 11:00:21 +0100

    search in export file

--- js/components/searchPrices.js
+++ js/components/searchPrices.js
@@ -1,21 +1,29 @@
 import { createHTML } from "./createHTML.js";
+import { displayMessage } from "./displayMessage.js";
 
-export function searchPrices(itemsToRender) {
-    const search = document.querySelector(".search");
+export function searchPrices(itemsToRender, event) {
+    const searchValue = event.target.value.trim();
+    const itemContainer = document.querySelector(".item-container")
 
-        search.onkeyup = function () {
-            const searchValue = this.value.trim();
-        
-            const filteredPrices = itemsToRender.filter(function(item) {
-                if (parseFloat(item.price) <= searchValue) {
-                    return true
-                }
-            })
-
-            if (searchValue === undefined) {
-                createHTML(results.data)
-            } else {
-                createHTML(filteredPrices)
+    
+    if (isNaN(searchValue)) {
+        // check if searchValue is string or numbers
+        displayMessage("Please use numbers", itemContainer);
+    } else {
+        // filter prices, if numbers are used
+        const filteredPrices = itemsToRender.filter(function(item) {
+            // convert string prices to float
+            if (parseFloat(item.price) <= searchValue) {
+                return true
             }
+        })
+        
+        if (filteredPrices.length === 0) {
+            // add initial results if searchValue is empty
+            createHTML(itemsToRender)
+        } else {
+            createHTML(filteredPrices)
         }
+    }
+  
 }
--- js/script.js
+++ js/script.js
@@ -1,23 +1,31 @@
 import { createHTML } from "./components/createHTML.js";
-import { searchPrices } from "./components/searchPrices.js";
 import { displayMessage } from "./components/displayMessage.js";
+import { searchPrices } from "./components/searchPrices.js";
 
 const url = "https://t9jt3myad3.execute-api.eu-west-2.amazonaws.com/api/products";
+const itemContainer = document.querySelector(".item-container");
 
 async function apiCall() {
+    // add loading message
+    displayMessage("Loading prices...", itemContainer);
+
     try {
         const response = await fetch(url);
         const results = await response.json(response);
+        // html based on results
         createHTML(results.data)
         let itemsToRender = results.data;
         
+        // search max prices end display results
         const search = document.querySelector(".search");
-        searchPrices(itemsToRender);
+        search.onkeyup = function(event) {
+            searchPrices(itemsToRender, event)
+        };
 
 
     }
     catch (error) {
-        console.log(error)
+        displayMessage(error, itemContainer)
     }
 }
 
